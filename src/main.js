e045501f1c5ef16c3392a82ebe87dd6c32f7dccf
bvaughn <brian.david.vaughn@gmail.com>
2015-04-01 22:46:13 -0700

    Updated promise matchers to be 1.3 and 2.x compatible.

--- src/main.js
+++ src/main.js
@@ -1,69 +1,101 @@
-/**
- * Matcher helpers for tests involving Promises.
- * These matchers do not have access to $rootScope and so you must invoke the digest loop for them after setting up expectations.
- *
- * For example:
- *   expect(promise).toBeRejected();
- *   $rootScope.$apply();
- */
+(function() {
+  var $rootScope;
 
-beforeEach(function() {
-  jasmine.addMatchers({
+  beforeEach(inject(function($injector) {
+    $rootScope = $injector.get('$rootScope');
+  }));
+
+  var PROMISE_STATE = {
+    PENDING: 'pending',
+    REJECTED: 'rejected',
+    RESOLVED: 'resolved',
+  };
+
+  var getPromiseInfo = function(promise, expectedState, expectedData) {
+    var info = {};
+
+    promise.then(
+      function(data) {
+        info.actualData = data;
+        info.actualState = PROMISE_STATE.RESOLVED;
+      },
+      function(data) {
+        info.actualData = data;
+        info.actualState = PROMISE_STATE.REJECTED;
+      });
+
+    $rootScope.$apply(); // Trigger Promise resolution
+
+    info.message = 'Expected ' + info.actualState + ' to be ' + expectedState;
+    info.pass = info.actualState === expectedState;
+
+    if (expectedData !== undefined && info.pass) {
+      if (jasmine.ObjectContaining && expectedData instanceof jasmine.ObjectContaining) {
+        info.pass = expectedData.asymmetricMatch(info.actualData);
+      } else {
+        info.pass = angular.equals(info.actualData, expectedData);
+      }
+
+      var actual = typeof info.actualData === 'object' ? JSON.stringify(info.actualData) : info.actualData;
+      var expected = typeof expectedData === 'object' ? JSON.stringify(expectedData) : expectedData;
+
+      info.message = 'Expected ' + actual + ' to be ' + expected;
+    }
+
+    return info;
+  };
+
+  var jasmine1Matchers = {
+    toBeRejected: function() {
+      return getPromiseInfo(this.actual, PROMISE_STATE.REJECTED).pass;
+    },
+    toBeRejectedWith: function(expectedData) {
+      return getPromiseInfo(this.actual, PROMISE_STATE.REJECTED, expectedData).pass;
+    },
+    toBeResolved: function() {
+      return getPromiseInfo(this.actual, PROMISE_STATE.RESOLVED).pass;
+    },
+    toBeResolvedWith: function(expectedData) {
+      return getPromiseInfo(this.actual, PROMISE_STATE.RESOLVED, expectedData).pass;
+    }
+  };
+
+  var jasmine2Matchers = {
     toBeRejected: function() {
       return {
         compare: function(promise) {
-          promise.then(
-            function() {
-              expect('Promise').toBe('rejected');
-            },
-            angular.noop);
-
-          return { pass: true };
+          return getPromiseInfo(promise, PROMISE_STATE.REJECTED);
         }
       };
     },
     toBeRejectedWith: function() {
       return {
-        compare: function(promise, expected) {
-          promise.then(
-            function() {
-              expect('Promise').toBe('rejected');
-            },
-            function(actual) {
-              expect(actual).toEqual(expected);
-            });
-
-          return { pass: true };
+        compare: function(promise, expectedData) {
+          return getPromiseInfo(promise, PROMISE_STATE.REJECTED, expectedData);
         }
       };
     },
     toBeResolved: function() {
       return {
         compare: function(promise) {
-          promise.then(
-            angular.noop,
-            function() {
-              expect('Promise').toBe('resolved');
-          });
-
-          return { pass: true };
+          return getPromiseInfo(promise, PROMISE_STATE.RESOLVED);
         }
       };
     },
     toBeResolvedWith: function() {
       return {
-        compare: function(promise, expected) {
-          promise.then(
-            function(actual) {
-              expect(actual).toEqual(expected);
-            },
-            function() {
-              expect('Promise').toBe('resolved');
-            });
-
-          return { pass: true };
+        compare: function(promise, expectedData) {
+          return getPromiseInfo(promise, PROMISE_STATE.RESOLVED, expectedData);
         }
       };
     }
+  };
+
+  beforeEach(function() {
+    if (/^2/.test(jasmine.version)) {
+      jasmine.addMatchers(jasmine2Matchers);
+    } else {
+      this.addMatchers(jasmine1Matchers);
+    }
   });
-});
+})();
--- test/unit/main.js
+++ test/unit/main.js
@@ -2,90 +2,72 @@
 
 describe('Promise Matcher tests', function () {
 
-  var $q;
-  var $rootScope;
+  var deferred;
 
-  beforeEach(inject(function($injector) {
-    $q = $injector.get('$q');
-    $rootScope = $injector.get('$rootScope');
+  beforeEach(inject(function($q) {
+    deferred = $q.defer();
   }));
 
   afterEach(inject(function($injector) {
-    $rootScope.$apply();
   }));
 
   it('should recognized already-resolved promises as being resolved', function() {
-    var deferred = $q.defer();
     deferred.resolve();
 
     expect(deferred.promise).toBeResolved();
   });
 
   it('should recognized already-resolved promises as being resolved with expected arguments', function() {
-    var deferred = $q.defer();
     deferred.resolve('foobar');
 
     expect(deferred.promise).toBeResolvedWith('foobar');
   });
 
-  it('should recognized to-be-resolved promises as being resolved', function() {
-    var deferred = $q.defer();
+  it('should not accept rejected promises as resolved', function() {
+    deferred.reject();
 
-    expect(deferred.promise).toBeResolved();
-
-    deferred.resolve();
+    expect(deferred.promise).not.toBeResolved();
   });
 
-  it('should recognized to-be-resolved promises as being resolved with expected arguments', function() {
-    var deferred = $q.defer();
+  it('should not accept promises resolved with unexpected arguments', function() {
+    deferred.resolve('foo');
 
-    expect(deferred.promise).toBeResolvedWith('foobar');
-
-    deferred.resolve('foobar');
+    expect(deferred.promise).not.toBeResolvedWith('bar');
   });
 
   it('should allow usage of matchers in expectations in already-resolved', function() {
-    var deferred = $q.defer();
     deferred.resolve({someProperty: 'someValue', somethingElse: 'dontCare'});
 
     expect(deferred.promise).toBeResolvedWith(jasmine.objectContaining({someProperty: 'someValue'}));
   });
 
   it('should recognized already-rejected promises as being rejected', function() {
-    var deferred = $q.defer();
     deferred.reject();
 
     expect(deferred.promise).toBeRejected();
   });
 
   it('should recognized already-rejected promises as being rejected with expected arguments', function() {
-    var deferred = $q.defer();
     deferred.reject('foobar');
 
     expect(deferred.promise).toBeRejectedWith('foobar');
   });
 
-  it('should recognized to-be-rejected promises as being rejected', function() {
-    var deferred = $q.defer();
-
-    expect(deferred.promise).toBeRejected();
-
-    deferred.reject();
-  });
-
   it('should allow usage of matchers in expectations in already-rejected', function() {
-    var deferred = $q.defer();
     deferred.reject({someProperty: 'someValue', somethingElse: 'dontCare'});
 
     expect(deferred.promise).toBeRejectedWith(jasmine.objectContaining({someProperty: 'someValue'}));
   });
 
-  it('should recognized to-be-rejected promises as being rejected with expected arguments', function() {
-    var deferred = $q.defer();
+  it('should not accept resolved promises as rejected', function() {
+    deferred.resolve();
 
-    expect(deferred.promise).toBeRejectedWith('foobar');
-
-    deferred.reject('foobar');
+    expect(deferred.promise).not.toBeRejected();
   });
 
+  it('should not accept promises rejected with unexpected arguments', function() {
+    deferred.reject('foo');
+
+    expect(deferred.promise).not.toBeRejectedWith('bar');
+  });
 });
