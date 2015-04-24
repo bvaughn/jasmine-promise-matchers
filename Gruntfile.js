bd21eb1703349fbb23692e0658a73cc30b1babde
bvaughn <brian.david.vaughn@gmail.com>
2015-04-24 08:36:22 -0700

    Jasmine 1.3 and 2.2 tests are runnable from main Gruntfile now

--- Gruntfile.js
+++ Gruntfile.js
@@ -21,13 +21,33 @@ module.exports = function(grunt) {
         src: 'dist/jasmine-promise-matchers.js',
         dest: 'dist/jasmine-promise-matchers.min.js'
       }
+    },
+    shell: {
+      jasmine1: {
+        command: 'npm run test',
+        options: {
+          execOptions: {
+            cwd: 'jasmine-1.3'
+          }
+        }
+      },
+      jasmine2: {
+        command: 'npm run test',
+        options: {
+          execOptions: {
+            cwd: 'jasmine-2.2'
+          }
+        }
+      }
     }
   });
 
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-shell');
 
-  grunt.registerTask('build', ['jshint','concat','uglify']);
-  grunt.registerTask('default', ['build']);
+  grunt.registerTask('build', ['jshint','test', 'concat','uglify']);
+  grunt.registerTask('test', ['shell']);
+  grunt.registerTask('default', ['test']);
 };
